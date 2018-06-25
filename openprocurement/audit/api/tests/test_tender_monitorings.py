4a49004f942302c4a7740514c3adae334383a410
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2018-06-25 21:34:44 +0300

    Tender monitorings list

---
+++ openprocurement/audit/api/tests/test_tender_monitorings.py
@@ -0,0 +1,87 @@
+from openprocurement.audit.api.tests.base import BaseWebTest
+from openprocurement.audit.api.design import MONITORINGS_BY_TENDER_FIELDS
+import unittest
+
+
+class TenderMonitoringsResourceTest(BaseWebTest):
+
+    def test_get_empty_list(self):
+        response = self.app.get('/tender/f9f9f9/monitorings')
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['data'], [])
+
+    def test_get(self):
+        tender_id = "f" * 32
+
+        ids = []
+        for i in range(10):
+            self.create_monitoring(tender_id=tender_id)
+            ids.append(self.monitoring_id)
+
+        for i in range(5):  # these are not on the list
+            self.create_monitoring(tender_id="a" * 32)
+
+        response = self.app.get('/tender/{}/monitorings'.format(tender_id))
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual([e["id"] for e in response.json['data']], ids)
+        self.assertEqual(set(response.json['data'][0].keys()),
+                         {"id", "dateCreated", "status"})
+
+    def test_get_custom_fields(self):
+        tender_id = "a" * 32
+
+        ids = []
+        for i in range(10):
+            self.create_monitoring(tender_id=tender_id)
+            ids.append(self.monitoring_id)
+
+        response = self.app.get(
+            '/tender/{}/monitorings?opt_fields=dateModified%2Creasons'.format(
+                tender_id
+            )
+        )
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+
+        self.assertEqual([e["id"] for e in response.json['data']], ids)
+        self.assertEqual(set(response.json['data'][0].keys()),
+                         {"id", "dateCreated", "dateModified", "status", "reasons"})
+
+    def test_get_test_empty(self):
+        tender_id = "a" * 32
+        for i in range(10):
+            self.create_monitoring(tender_id=tender_id, mode="test")
+
+        response = self.app.get(
+            '/tender/{}/monitorings'.format(tender_id)
+        )
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['data'], [])
+
+    def test_get_test(self):
+        tender_id = "a" * 32
+
+        ids = []
+        for i in range(10):
+            self.create_monitoring(tender_id=tender_id, mode="test")
+            ids.append(self.monitoring_id)
+
+        response = self.app.get(
+            '/tender/{}/monitorings?mode=test'.format(tender_id)
+        )
+        self.assertEqual(response.status, '200 OK')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual([e["id"] for e in response.json['data']], ids)
+
+
+def suite():
+    s = unittest.TestSuite()
+    s.addTest(unittest.makeSuite(TenderMonitoringsResourceTest))
+    return s
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')

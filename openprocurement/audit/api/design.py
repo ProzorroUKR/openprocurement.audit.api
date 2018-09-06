cc5bbe05d0dcec08339843195a3f834c259b322f
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2018-09-06 10:58:01 +0300

    CS-1034 hide cancelled monitorings from public feeds

--- openprocurement/audit/api/design.py
+++ openprocurement/audit/api/design.py
@@ -25,7 +25,7 @@ monitorings_all_view = ViewDefinition('monitorings', 'all', '''function(doc) {
 }''')
 
 monitorings_by_dateModified_view = ViewDefinition('monitorings', 'by_dateModified', '''function(doc) {
-    if(doc.doc_type == 'Monitoring' && doc.status != 'draft') {
+    if(doc.doc_type == 'Monitoring' && ['draft', 'cancelled'].indexOf(doc.status) == -1) {
         var fields=%s, data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
@@ -37,7 +37,7 @@ monitorings_by_dateModified_view = ViewDefinition('monitorings', 'by_dateModifie
 }''' % FIELDS)
 
 monitorings_real_by_dateModified_view = ViewDefinition('monitorings', 'real_by_dateModified', '''function(doc) {
-    if(doc.doc_type == 'Monitoring' && !doc.mode && doc.status != 'draft') {
+    if(doc.doc_type == 'Monitoring' && !doc.mode && ['draft', 'cancelled'].indexOf(doc.status) == -1) {
         var fields=%s, data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
@@ -49,7 +49,7 @@ monitorings_real_by_dateModified_view = ViewDefinition('monitorings', 'real_by_d
 }''' % FIELDS)
 
 monitorings_test_by_dateModified_view = ViewDefinition('monitorings', 'test_by_dateModified', '''function(doc) {
-    if(doc.doc_type == 'Monitoring' && doc.mode == 'test' && doc.status != 'draft') {
+    if(doc.doc_type == 'Monitoring' && doc.mode == 'test' && ['draft', 'cancelled'].indexOf(doc.status) == -1) {
         var fields=%s, data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
@@ -85,7 +85,7 @@ monitorings_all_draft_by_dateModified_view = ViewDefinition('monitorings', 'draf
 }''' % FIELDS)
 
 monitorings_by_local_seq_view = ViewDefinition('monitorings', 'by_local_seq', '''function(doc) {
-    if(doc.doc_type == 'Monitoring' && doc.status != 'draft') {
+    if(doc.doc_type == 'Monitoring' && ['draft', 'cancelled'].indexOf(doc.status) == -1) {
         var fields=%s, data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
@@ -97,7 +97,7 @@ monitorings_by_local_seq_view = ViewDefinition('monitorings', 'by_local_seq', ''
 }''' % CHANGES_FIELDS)
 
 monitorings_real_by_local_seq_view = ViewDefinition('monitorings', 'real_by_local_seq', '''function(doc) {
-    if(doc.doc_type == 'Monitoring' && !doc.mode && doc.status != 'draft') {
+    if(doc.doc_type == 'Monitoring' && !doc.mode && ['draft', 'cancelled'].indexOf(doc.status) == -1) {
         var fields=%s, data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
@@ -109,7 +109,7 @@ monitorings_real_by_local_seq_view = ViewDefinition('monitorings', 'real_by_loca
 }''' % CHANGES_FIELDS)
 
 monitorings_test_by_local_seq_view = ViewDefinition('monitorings', 'test_by_local_seq', '''function(doc) {
-    if(doc.doc_type == 'Monitoring' && doc.mode == 'test' && doc.status != 'draft') {
+    if(doc.doc_type == 'Monitoring' && doc.mode == 'test' && ['draft', 'cancelled'].indexOf(doc.status) == -1) {
         var fields=%s, data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
@@ -150,7 +150,7 @@ MONITORINGS_BY_TENDER_FIELDS = [
 ]
 
 monitorings_by_tender_id_view = ViewDefinition('monitorings', 'by_tender_id', '''function(doc) {
-    if(doc.doc_type == 'Monitoring' && !doc.mode && doc.status != 'draft') {
+    if(doc.doc_type == 'Monitoring' && !doc.mode && ['draft', 'cancelled'].indexOf(doc.status) == -1) {
         var fields=%s, data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
@@ -162,7 +162,7 @@ monitorings_by_tender_id_view = ViewDefinition('monitorings', 'by_tender_id', ''
 }''' % MONITORINGS_BY_TENDER_FIELDS)
 
 test_monitorings_by_tender_id_view = ViewDefinition('monitorings', 'test_by_tender_id', '''function(doc) {
-    if(doc.doc_type == 'Monitoring' && doc.mode == 'test' && doc.status != 'draft') {
+    if(doc.doc_type == 'Monitoring' && doc.mode == 'test' && ['draft', 'cancelled'].indexOf(doc.status) == -1) {
         var fields=%s, data={};
         for (var i in fields) {
             if (doc[fields[i]]) {
--- openprocurement/audit/api/tests/test_tender_monitorings.py
+++ openprocurement/audit/api/tests/test_tender_monitorings.py
@@ -1,4 +1,5 @@
 from openprocurement.audit.api.tests.base import BaseWebTest
+from openprocurement.audit.api.constants import CANCELLED_STATUS, ACTIVE_STATUS
 import unittest
 from datetime import datetime
 
@@ -29,6 +30,86 @@ class TenderMonitoringsResourceTest(BaseWebTest):
         self.assertEqual([e["id"] for e in response.json['data']], ids)
         self.assertEqual(set(response.json['data'][0].keys()),
                          {"id", "dateCreated", "status"})
+
+    def test_get_without_draft(self):
+        tender_id = "f" * 32
+        self.app.authorization = ('Basic', (self.sas_token, ''))
+
+        self.create_monitoring(tender_id=tender_id)
+        draft_id = self.monitoring_id
+
+        self.create_monitoring(tender_id=tender_id)
+        cancelled_id = self.monitoring_id
+        self.app.patch_json(
+            '/monitorings/{}'.format(self.monitoring_id),
+            {'data': {
+                "status": CANCELLED_STATUS,
+                'cancellation': {
+                    'description': 'some_description'
+                }
+            }})
+
+        self.create_monitoring(tender_id=tender_id)
+        active_id = self.monitoring_id
+        self.app.patch_json(
+            '/monitorings/{}'.format(self.monitoring_id),
+            {"data": {
+                "decision": {"description": "text"},
+                "status": ACTIVE_STATUS,
+            }}
+        )
+
+        response = self.app.get('/tenders/{}/monitorings'.format(tender_id))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual([e["id"] for e in response.json['data']], [active_id])
+        self.assertEqual(set(response.json['data'][0].keys()),
+                         {"id", "dateCreated", "status"})
+
+    def test_get_with_draft(self):
+        tender_id = "f" * 32
+        self.app.authorization = ('Basic', (self.sas_token, ''))
+
+        self.create_monitoring(tender_id=tender_id)
+        draft_id = self.monitoring_id
+
+        self.create_monitoring(tender_id=tender_id)
+        cancelled_id = self.monitoring_id
+        self.app.patch_json(
+            '/monitorings/{}'.format(self.monitoring_id),
+            {'data': {
+                "status": CANCELLED_STATUS,
+                'cancellation': {
+                    'description': 'some_description'
+                }
+            }})
+
+        self.create_monitoring(tender_id=tender_id)
+        active_id = self.monitoring_id
+        self.app.patch_json(
+            '/monitorings/{}'.format(self.monitoring_id),
+            {"data": {
+                "decision": {"description": "text"},
+                "status": ACTIVE_STATUS,
+            }}
+        )
+
+        response = self.app.get('/tenders/{}/monitorings?mode=draft'.format(tender_id))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual({e["id"] for e in response.json['data']}, {active_id, draft_id, cancelled_id})
+        self.assertEqual(set(response.json['data'][0].keys()),
+                         {"id", "dateCreated", "status"})
+
+    def test_get_with_draft_forbidden(self):
+        tender_id = "f" * 32
+        self.app.authorization = None
+        response = self.app.get('/tenders/{}/monitorings?mode=draft'.format(tender_id), status=403)
+        self.assertEqual(
+            response.json,
+            {u'status': u'error',
+             u'errors': [{u'description': u'Forbidden', u'location': u'url', u'name': u'permission'}]}
+        )
 
     def test_get_custom_fields(self):
         tender_id = "a" * 32

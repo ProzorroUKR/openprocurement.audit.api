ff4cdc9a506fcda39c7f095c4dba2f70ac9248be
stepan.rasputny <stepan.rasputny@raccoongang.com>
2020-11-24 11:40:35 +0200

    Moved calendar logic to standards package

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ requires = [
     'rfc6266==0.0.6',
     'openprocurement_client==2.1.1dp',
     'CouchDB==1.2-bad-status-line',
+    'standards>=1.0.3',
 ]
 test_requires = requires + [
     'pytest',

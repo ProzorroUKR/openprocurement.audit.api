06024846d8687631124fd4150f130c15ba721ec5
Oleksandr Hrekov <alexander.greckov@prozori.in.ua>
2026-09-07 15:59:49 +0300

    Fix sentry sdk_error and upgrade this library

--- openprocurement/audit/api/app.py
+++ openprocurement/audit/api/app.py
@@ -40,7 +40,7 @@ def main(global_config, **settings):
                 PyramidIntegration(),
             ],
             send_default_pii=True,
-            request_bodies="always",
+            max_request_body_size="always",
             environment=settings.get("sentry.environment"),
         )
 
